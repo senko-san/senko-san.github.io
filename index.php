969a6c18e026e541c327df3ce6be9ec6274757fa
Thighs <Senko@waifu.club>
2019-05-13 14:37:26 -0700

    FileUploadTest

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
         <title></title>
 </head>
 <body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <button type="submit" name="submit">UPLOAD
+</button>
+</form>
 </body>
 </html>
 
